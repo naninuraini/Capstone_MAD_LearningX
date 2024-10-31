d39d231469bf29eba404a11ba98ff168f0916696
ziro <dzikrirofa@gmail.com>
2024-10-31 11:45:12 +0700

    log out from home page

--- cipta_cuan/lib/app/modules/home/controllers/home_controller.dart
+++ cipta_cuan/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
+import '../../login/views/login_view.dart';
+
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  FirebaseAuth auth = FirebaseAuth.instance;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void logout() async {
+    await auth.signOut();
+    Get.off(() => const LoginView());
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
