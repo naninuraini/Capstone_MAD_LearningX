2e7cadcda550d5d44c8fec6cb2b30f79efcf4d5f
ziro <dzikrirofa@gmail.com>
2024-10-31 11:46:34 +0700

    add some page

--- cipta_cuan/lib/app/routes/app_routes.dart
+++ cipta_cuan/lib/app/routes/app_routes.dart
@@ -4,9 +4,15 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
+  static const LUPA_PASSWORD = _Paths.LUPA_PASSWORD;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const LUPA_PASSWORD = '/lupa-password';
 }
