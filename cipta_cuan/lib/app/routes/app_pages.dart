bfe427ae5b600e84d17d6be3ef3928f619481349
naninuraini <naninuraini1023@gmail.com>
2024-11-25 01:13:21 +0700

    menambahkan pages

--- cipta_cuan/lib/app/routes/app_pages.dart
+++ cipta_cuan/lib/app/routes/app_pages.dart
@@ -10,10 +10,14 @@ import '../modules/lupa_password/bindings/lupa_password_binding.dart';
 import '../modules/lupa_password/views/lupa_password_view.dart';
 import '../modules/profil/bindings/profil_binding.dart';
 import '../modules/profil/views/profil_view.dart';
+import '../modules/profil_avatar/bindings/profil_avatar_binding.dart';
+import '../modules/profil_avatar/views/profil_avatar_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
+import '../modules/tentang_kami/bindings/tentang_kami_binding.dart';
+import '../modules/tentang_kami/views/tentang_kami_view.dart';
 
 part 'app_routes.dart';
 
@@ -57,6 +61,16 @@ class AppPages {
       name: _Paths.DETAIL_PENGGUNA,
       page: () => const DetailPenggunaView(),
       binding: DetailPenggunaBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFIL_AVATAR,
+      page: () => const ProfilAvatarView(),
+      binding: ProfilAvatarBinding(),
+    ),
+    GetPage(
+      name: _Paths.TENTANG_KAMI,
+      page: () => const TentangKamiView(),
+      binding: TentangKamiBinding(),
     ),
   ];
 }
