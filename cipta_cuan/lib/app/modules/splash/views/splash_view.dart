f4624b5100fd1ccf9ffd20e717592d230fd7d700
ziro <dzikrirofa@gmail.com>
2024-11-25 00:18:39 +0700

    create splash screen and add authentication on splash screen

--- cipta_cuan/lib/app/modules/splash/views/splash_view.dart
+++ cipta_cuan/lib/app/modules/splash/views/splash_view.dart
@@ -1,28 +1,40 @@
+import 'dart:developer';
+
+import 'package:cipta_cuan/widget/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import '../../../routes/app_pages.dart';
 import '../controllers/splash_controller.dart';
-import 'splash_view1.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
   @override
   Widget build(BuildContext context) {
-    // Beri waktu 3 detik sebelum berpindah ke screen berikutnya
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.off(() => const SplashScreen1()); // Pindah ke splash screen pertama
-    });
-
+    controller.checkOnboardingStatus();
     return Scaffold(
-      backgroundColor: const Color(0xFF0A0E21), // Warna biru navy
       body: Center(
-        child: Text(
-          'CiptaCuan',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.blueAccent, // Sesuaikan warna teks sesuai gambar
-          ),
+        child: StreamBuilder<User?>(
+          stream: controller.userStream,
+          builder: (context, snapshot) {
+            log("$snapshot");
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return LoadingView();
+            } else if (snapshot.hasData && snapshot.data!.emailVerified == true) {
+              Future.microtask(() => Get.offAllNamed('/home'));
+            } else {
+              Future.microtask(() => Get.offAllNamed(Routes.ON_BOARDING));
+            }
+            return Text(
+              'CiptaCuan',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            );
+          }
         ),
       ),
     );
