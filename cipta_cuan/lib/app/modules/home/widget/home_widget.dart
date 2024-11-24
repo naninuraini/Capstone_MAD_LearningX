f60f297eba38a00056305a71d0a21055cd406e71
ziro <dzikrirofa@gmail.com>
2024-11-25 01:34:56 +0700

    create bottom navigation

---
+++ cipta_cuan/lib/app/modules/home/widget/home_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeWidget extends GetView<HomeController> {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.logout();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+          IconButton(
+            onPressed: () {
+              Get.toNamed('/profil');
+            },
+            icon: const Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
