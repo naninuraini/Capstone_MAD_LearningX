0c88fc92687b80b8ff0b15c20cc017ae60e6f4a1
ziro <dzikrirofa@gmail.com>
2024-12-04 00:07:59 +0700

    add tab controller andd tabs

--- cipta_cuan/lib/app/modules/home/controllers/home_controller.dart
+++ cipta_cuan/lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../login/views/login_view.dart';
 
 class HomeController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
+  late TabController tabController;
+  final selectedColor = Color(0xff1a73e8);
+  // final unselectedColor = Color(0xff5f6368);
+  final tabs = [
+    Tab(text: 'Harian'),
+    Tab(text: 'Mingguan'),
+    Tab(text: 'Tahunan'),
+  ];
 
   void logout() async {
     await auth.signOut();
