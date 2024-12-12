501f948fd24f185e1840b87782228dcc96d0e5b5
naninuraini <naninuraini1023@gmail.com>
2024-12-12 17:51:53 +0700

    menghapus

--- cipta_cuan/lib/app/modules/on_boarding/views/on_boarding_view.dart
+++ cipta_cuan/lib/app/modules/on_boarding/views/on_boarding_view.dart
@@ -13,35 +13,6 @@ class OnBoardingView extends StatelessWidget {
         onPageChanged: (index) => controller.currentPage.value = index,
         children: controller.buildPages(),
       ),
-      // bottomNavigationBar: 
-      // controller.currentPage.value < 2
-      //     ? Obx(() {
-      //         final isLastPage = controller.currentPage.value ==
-      //             controller.buildPages().length - 1;
-
-              // return Padding(
-              //   padding: const EdgeInsets.all(16.0),
-              //   child: ElevatedButton(
-              //     onPressed: () {
-              //       controller.pageController.nextPage(
-              //         duration: const Duration(milliseconds: 300),
-              //         curve: Curves.ease,
-              //       );
-              //     },
-              //     style: ElevatedButton.styleFrom(
-              //       backgroundColor: const Color(0xFF0DA6C2),
-              //       shape: RoundedRectangleBorder(
-              //         borderRadius: BorderRadius.circular(12),
-              //       ),
-              //     ),
-              //     child: Text(
-              //       'Next',
-              //       style: const TextStyle(fontSize: 18, color: Colors.white),
-              //     ),
-              //   ),
-              // );
-    //         })
-    //       : Container(),
     );
   }
 }
