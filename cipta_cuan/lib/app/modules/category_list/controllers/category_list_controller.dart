cc194c6b09b9295db4ea7b041e09540767db4355
jremyeric <135425915+jremyeric@users.noreply.github.com>
2024-12-13 11:19:32 +0800

    styling 'category list'

--- cipta_cuan/lib/app/modules/category_list/controllers/category_list_controller.dart
+++ cipta_cuan/lib/app/modules/category_list/controllers/category_list_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import '../../../../models/post/post_model.dart';
 import '../../../../models/post/post_entity.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,6 +8,18 @@ class CategoryListController extends GetxController {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   var isLoading = false.obs;
   var posts = <Post>[].obs;
+
+  final categories = [
+    {"icon": "assets/category/konsumsi.png", "label": "Konsumsi"},
+    {"icon": "assets/category/transportasi.png", "label": "Transportasi"},
+    {"icon": "assets/category/obat.png", "label": "Obat-Obatan"},
+    {"icon": "assets/category/makanan.png", "label": "Bahan Makanan"},
+    {"icon": "assets/category/sewa.png", "label": "Sewa"},
+    {"icon": "assets/category/hadiah.png", "label": "Hadiah"},
+    {"icon": "assets/category/tabungan.png", "label": "Tabungan"},
+    {"icon": "assets/category/hiburan.png", "label": "Hiburan"},
+    {"icon": "assets/category/lainnya.png", "label": "Lainnya"},
+  ];
 
   void getCategoryPosts(String myUserId, String categoryLabel) async {
     try {
@@ -33,7 +46,16 @@ class CategoryListController extends GetxController {
       Get.snackbar('Error', 'Failed to load category posts.');
     }
   }
-
+  
+  String formatRupiah(num number, String symbol) {
+    final formatter = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: symbol,
+      decimalDigits: 0,
+    );
+    return formatter.format(number);
+  }
+  
   @override
   void onInit() {
     super.onInit();
